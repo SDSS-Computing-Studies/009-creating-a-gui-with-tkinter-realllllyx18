102601e163be71867e622ee9b6ca7bedff7cf626
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-11-01 16:35:00 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -2,12 +2,17 @@ import tkinter as tk
 from tkinter import *
 
 window = tk.Tk()
-window.title("tk")
 
-lable1 = tk.lable(window,text="x")
-lable2 = tk.lable(window,text="=")
+label1 = tk.Label(window,text="x")
+button1 = tk.Button(window,text="=")
+entry1 = tk.Entry(window, borderwidth=3)
+entry2 = tk.Entry(window, borderwidth=3)
+entry3 = tk.Entry(window, borderwidth=3)
 
-label1.grid(row = 1, column = 1)
-label2.grid(row = 1, column = 1)
+entry1.grid (row = 1, column = 1)
+label1.grid (row = 1, column = 2)
+entry2.grid (row = 1, column = 3)
+button1.grid (row = 1, column =4)
+entry3.grid (row = 1, column = 5)
 
 window.mainloop()
