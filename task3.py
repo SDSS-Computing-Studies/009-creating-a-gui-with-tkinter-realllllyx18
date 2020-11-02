e65bf09d9a7b28d8aa447cbdf6602af4e4f8b234
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-11-02 11:43:21 -0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -5,16 +5,16 @@ from tkinter import *
 from tkinter import ttk
 
 window = tk.Tk()
-window.title("tk")
-window.geometry("280x135")
+window.title("example")
+window.geometry("315x135")
 
 dogphoto = PhotoImage(file="dog.png")
 
 label1 = tk.Label(window, image=dogphoto)
 label2 = tk.Label(window,text = "Pochacco!")
-label3 = tk.Label(window,text="A cuddly little puppy! This is from the same creators \n who brought you Keropi and Kero Kero", bg="#50FFFA")
-label1.place(x=100,y=0)
-label2.place(x=180,y=50)
+label3 = tk.Label(window,text="            A cuddly little puppy! This is from the same creators \n   who brought you Keropi and Kero Kero", bg="#50FFFA")
+label1.place(x=80,y=0)
+label2.place(x=160,y=30)
 label3.place(x=0,y=100)
 
 window.mainloop()
